0cc0ae050fee9eb9ef641c05874862a5cb906857
alireza <121108490+alireza64ahmadi@users.noreply.github.com>
2024-03-15 17:44:25 +0330

    Enhance Element Generator with Child Elements (Level Up)
     Expanded the elementGenerator function to include child elements for enhanced flexibility in creating complex HTML structures. The function now accepts a variable number of child elements in addition to the tagName and attributes. It loops through the provided child elements and appends them to the created element, allowing for the creation of nested elements easily.

--- JS/index.js
+++ JS/index.js
@@ -114,3 +114,31 @@ function elementGenerator(tagName, attrbiutes) {
 
     return element
 }
+
+// STEP5: add newFeature to element Generator (LevelUP)
+
+// Dynamic Element Generator
+function elementGenerator(tagName, attrbiutes, ...childs) {
+    // Create Element
+    const element = document.createElement(tagName)
+
+    // Does 'Attrbiutes' parameter exist?
+    if (attrbiutes) {
+        Object.keys(attrbiutes).forEach(key => {
+            element.setAttribute(key, attrbiutes[key])
+        })
+    }
+
+    // Does 'childs' parameter exist?
+    if (childs) {
+        childs.forEach(child => {
+            if (typeof child === 'string') {
+                element.appendChild(document.createTextNode(child))
+            } else {
+                element.appendChild(child)
+            }
+        })
+    }
+
+    return element
+}
